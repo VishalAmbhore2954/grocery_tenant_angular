66b7976983d2b9e8a41d3f662221eaa4630c05eb
Vishal Ambhore <vishalambhore2954@gmail.com>
2026-04-01 14:31:45 +0530

    change

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('grocery_store_frontend');
+  protected readonly title = signal('Something big is on the way');
 }
